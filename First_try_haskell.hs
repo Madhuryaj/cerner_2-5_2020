ebaba8b70ab4b75659fc6d8a7b51ad12672993f3
JMadhurya <71380361+JMadhurya@users.noreply.github.com>
2020-09-17 21:27:24 +0530

    Add files via upload

---
+++ First_try_haskell.hs
@@ -0,0 +1,6 @@
+{-|Haskell Programming language "cerner_2^5_2020"
+-}
+main = do  
+    putStrLn "Hello, I am doing 17th Sept code submission.Can you Know your name :) "  
+    name <- getLine  
+    putStrLn ("Hey " ++ name ++ ", you rock!, Thanks for looking into my code.") 
