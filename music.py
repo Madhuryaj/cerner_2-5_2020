bcdfa34fcadd2de94f5b0efa8f56a45958c6861c
JMadhurya <71380361+JMadhurya@users.noreply.github.com>
2020-10-08 23:27:30 +0530

    Add files via upload

---
+++ music.py
@@ -0,0 +1,17 @@
+from pygame import mixer
+
+mixer.init()
+mixer.music.load("Song_path.mp3")
+mixer.music.play() #"cerner_2^5_2020"
+
+while True:# infinite loop
+    print("Press \nP to Pause\nR to Resume\nE to exit the program")
+    user_input_query = input(" ")
+
+    if user_input_query == 'P':
+        mixer.music.pause()
+    elif user_input_query == 'R':
+        mixer.music.unpause()
+    elif user_input_query == 'E':
+        mixer.music.stop()
+        break #"cerner_2^5_2020"
