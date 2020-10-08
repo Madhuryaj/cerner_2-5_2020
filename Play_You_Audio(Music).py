7af0580391bdf6c77774eb682a17b3e1831230ba
JMadhurya <71380361+JMadhurya@users.noreply.github.com>
2020-10-08 23:29:04 +0530

    Update Play_You_Audio(Music).py

--- Play_You_Audio(Music).py
+++ Play_You_Audio(Music).py
@@ -7,7 +7,6 @@ mixer.music.play() #"cerner_2^5_2020"
 while True:# infinite loop
     print("Press \nP to Pause\nR to Resume\nE to exit the program")
     user_input_query = input(" ")
-
     if user_input_query == 'P':
         mixer.music.pause()
     elif user_input_query == 'R':
